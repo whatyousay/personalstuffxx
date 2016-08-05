f44687b529386506a18574dd21d484c80ca5a659
whatyousay <whatyousay@users.noreply.github.com>
2016-08-05 10:57:53 +0800

    Update world.js

--- world.js
+++ world.js
@@ -4318,7 +4318,7 @@ GameController = (function()
 		{
 			var visionRadius = GlobalState.TileWHalf + (GlobalState.TileW * playerVision);
 
-			darkness.SetVisionRadius(1000);
+			darkness.SetVisionRadius(1250);
 			//mouseController.SetMaxDistance(visionRadius);
 			mouseController.SetMaxDistance(false);
 		}
